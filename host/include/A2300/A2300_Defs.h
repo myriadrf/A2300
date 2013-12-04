ce2f7d374e24904a1d38aa48b0e9a353d9b93a08
Michael Dickens <mlk@alum.mit.edu>
2013-12-04 11:15:33 -0500

    Add or fix license in all Loctronix copyright files

--- host/include/A2300/A2300_Defs.h
+++ host/include/A2300/A2300_Defs.h
@@ -1,11 +1,19 @@
-/************************************************************************
- * Name:  A2300_Defs.h
- * 
- * This module is the proprietary property of Loctronix Corporation
- * Copyright (C) 2013 Loctronix Corporation
- * All Rights Reserved
- * 
- ***************************************************************************/
+/** Name: A2300_Defs.h
+*
+* Copyright(c) 2013 Loctronix Corporation
+* http://www.loctronix.com
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
+
 #ifndef A2300_DEFS_H_
 #define A2300_DEFS_H_
 
--- host/include/A2300/BitUtil.h
+++ host/include/A2300/BitUtil.h
@@ -1,9 +1,18 @@
-/*
- * BitUtil.h
- *
- *  Created on: Nov 5, 2013
- *      Author: mike
- */
+/** Name: BitUtil.h
+*
+* Copyright(c) 2013 Loctronix Corporation
+* http://www.loctronix.com
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*/
 
 #ifndef BITUTIL_H_
 #define BITUTIL_H_
