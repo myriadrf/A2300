b002efe2ddd0b1ae569d96e1e1dadfd7ab6e6d5d
Michael Dickens <mlk@alum.mit.edu>
2013-12-16 08:48:11 -0500

    use explicit types in macro number definitions

--- host/include/A2300/A2300_Defs.h
+++ host/include/A2300/A2300_Defs.h
@@ -22,38 +22,38 @@
 
 
 // Loctronix System Defaults
-#define A2300_DEFAULT_FREQ      (1.946e9)   	// gHz -> mHz -> kHz
-#define A2300_DEFAULT_GAIN		(0.0)
-#define A2300_DEFAULT_TICK_RATE (32e6)		// 32-MHz.
+#define A2300_DEFAULT_FREQ      ((double)(1.946e9))   	// gHz -> mHz -> kHz
+#define A2300_DEFAULT_GAIN		((double)(0.0))
+#define A2300_DEFAULT_TICK_RATE ((double)(32e6))		// 32-MHz.
 
-#define A2300_RFFREQ_MIN        (0.3e9)         // 0.3 GHz.
-#define A2300_RFFREQ_MAX	    (3.8e9)		// 3.8 GHz.
-#define A2300_RFFREQ_STEP		(1000)
-#define A2300_SAMPFREQ_MAX		(32e6)
-#define A2300_SAMPFREQ_MIN      (7812.5)
-#define A2300_DDUC_FREQ_MAX     (14e6)
-#define A2300_DDUC_FREQ_MIN		(-14e6)
-#define A2300_DDUC_FREQ_STEP	(0.0149011611938)
+#define A2300_RFFREQ_MIN        ((double)(0.3e9))         // 0.3 GHz.
+#define A2300_RFFREQ_MAX	    ((double)(3.8e9))		// 3.8 GHz.
+#define A2300_RFFREQ_STEP		((double)(1000))
 
-#define A2300_MIN_BW			(0.75e6 * 2.0)	// 0.75 MHz.
-#define A2300_MAX_BW 			(14.0e6 * 2.0)	// 14.0 MHz.
+#define A2300_SAMPFREQ_MAX		((double)(32e6))
+#define A2300_SAMPFREQ_MIN      ((double)(7812.5))
 
-#define A2300_RXGAIN_MIN			(0)
-#define A2300_RXGAIN_MAX			(30)
-#define A2300_RXGAIN_STEP			(3)
+#define A2300_DDUC_FREQ_MAX     ((double)(14e6))
+#define A2300_DDUC_FREQ_MIN		((double)(-14e6))
+#define A2300_DDUC_FREQ_STEP	((double)(0.0149011611938))
 
-#define A2300_TXGAIN_MIN			(0)
-#define A2300_TXGAIN_MAX			(56)
-#define A2300_TXGAIN_STEP			(1)
+#define A2300_MIN_BW			((double)(0.75e6 * 2.0))	// 0.75 MHz.
+#define A2300_MAX_BW 			((double)(14.0e6 * 2.0))	// 14.0 MHz.
 
+#define A2300_RXGAIN_MIN			((double)(0))
+#define A2300_RXGAIN_MAX			((double)(30))
+#define A2300_RXGAIN_STEP			((double)(3))
 
-#define A2300_MAX_SAMPLING_FREQ (32.0e6) // 32-MHz.
-#define A2300_MIN_CLOCK   		(A2300_MAX_SAMPLING_FREQ/4096.0)
-#define A2300_MAX_CLOCK   		(A2300_MAX_SAMPLING_FREQ/1.0)
-#define A2300_SYSTEM_CLOCK		(100e6)
+#define A2300_TXGAIN_MIN			((double)(0))
+#define A2300_TXGAIN_MAX			((double)(56))
+#define A2300_TXGAIN_STEP			((double)(1))
 
-#define A2300_WAIT_TIME 		(0.1)
+#define A2300_MAX_SAMPLING_FREQ ((double)(32.0e6)) // 32-MHz.
+#define A2300_MIN_CLOCK   		(A2300_MAX_SAMPLING_FREQ/((double)4096.0))
+#define A2300_MAX_CLOCK   		(A2300_MAX_SAMPLING_FREQ/((double)1.0))
+#define A2300_SYSTEM_CLOCK		((double)(100e6))
 
+#define A2300_WAIT_TIME 		((double)(0.1))
 
 /**
  * Interface implemented components supporting device configuration
